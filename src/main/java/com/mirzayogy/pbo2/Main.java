4b1f2f4ae5794117ae24474d9a2180a61e98fc02
mirzayogy <mirza.yogy@gmail.com>
2021-11-03 22:00:52 +0800

    membuat model dan logic nya

--- src/main/java/com/mirzayogy/pbo2/Main.java
+++ src/main/java/com/mirzayogy/pbo2/Main.java
@@ -1,13 +1,57 @@
 package com.mirzayogy.pbo2;
 
+import com.mirzayogy.pbo2.model.JenisBarang;
 import com.mirzayogy.pbo2.model.Pengguna;
 import com.mirzayogy.pbo2.view.LoginFrame;
 import com.mirzayogy.pbo2.view.MainPublic;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        LoginFrame loginFrame = new LoginFrame();
-        loginFrame.setVisible(true);
+//        LoginFrame loginFrame = new LoginFrame();
+//        loginFrame.setVisible(true);
+
+        JenisBarang jenisBarang = new JenisBarang();
+        
+//        jenisBarang.setNamajenisbarang("Modul 5F");
+//        if(jenisBarang.create()){
+//            System.out.println("Simpan Berhasil");
+//        } else {
+//            System.out.println("Gagal Simpan");
+//        }
+
+
+//        jenisBarang.setId(16);
+//        jenisBarang.setNamajenisbarang("Modul 5F baru");
+//        if(jenisBarang.update()){
+//            System.out.println("Ubah Data Berhasil");
+//        } else {
+//            System.out.println("Gagal Ubah Data");
+//        }
+
+//        jenisBarang.setId(8);
+//        if(jenisBarang.delete()){
+//            System.out.println("Hapus Data Berhasil");
+//        } else {
+//            System.out.println("Gagal Hapus Data");
+//        }
+        
+//        jenisBarang.setId(3);
+//        if(jenisBarang.find()){
+//            System.out.println(jenisBarang.getNamajenisbarang());
+//        } else {
+//            System.out.println("Data tidak ditemukan");
+//        }
+
+//        ArrayList<JenisBarang> list = jenisBarang.read();
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i).getNamajenisbarang());
+//        }
+
+        ArrayList<JenisBarang> list = jenisBarang.search("pakai");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getNamajenisbarang());
+        }
     }
 }
---
+++ src/main/java/com/mirzayogy/pbo2/model/JenisBarang.java
@@ -0,0 +1,174 @@
+package com.mirzayogy.pbo2.model;
+
+import com.mirzayogy.pbo2.db.Database;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JenisBarang {
+    private int id;
+    private String namajenisbarang;
+    
+    private Database database;
+    private Connection connection;
+    
+    public boolean create(){
+        
+        String insertSQL = "INSERT INTO jenisbarang VALUES (NULL, ?)";
+        
+        this.database = new Database();
+        this.connection = this.database.getConnection();
+        
+        try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(insertSQL);
+            preparedStatement.setString(1, this.namajenisbarang);
+            preparedStatement.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(JenisBarang.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+    public boolean update(){
+        String updateSQL = "UPDATE jenisbarang SET namajenisbarang = ? WHERE id = ?";
+        
+        this.database = new Database();
+        this.connection = this.database.getConnection();
+        
+        try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(updateSQL);
+            preparedStatement.setString(1, this.namajenisbarang);
+            preparedStatement.setInt(2, this.id);
+            preparedStatement.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(JenisBarang.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
+    public boolean delete(){
+        String deleteSQL = "DELETE FROM jenisbarang WHERE id = ?";
+        
+        this.database = new Database();
+        this.connection = this.database.getConnection();
+        
+        try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(deleteSQL);
+            preparedStatement.setInt(1, this.id);
+            preparedStatement.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(JenisBarang.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
+    public ArrayList<JenisBarang> read(){
+        
+        ArrayList<JenisBarang> list = new ArrayList<>();
+        
+        String selectSQL = "SELECT * FROM jenisbarang" ;
+        
+        this.database = new Database();
+        this.connection = this.database.getConnection();
+        
+        try{
+            PreparedStatement preparedStatement = this.connection.prepareStatement(selectSQL);
+            ResultSet rs = preparedStatement.executeQuery();
+            
+            while(rs.next()){
+                JenisBarang jb = new JenisBarang();
+                jb.setId(rs.getInt("id"));
+                jb.setNamajenisbarang(rs.getString("namajenisbarang"));
+                
+                list.add(jb);
+            }
+            
+            return list;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(JenisBarang.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
+    
+    public boolean find(){
+        String findSQL = "SELECT * FROM jenisbarang WHERE id = ?";
+        
+        this.database = new Database();
+        this.connection = this.database.getConnection();
+        
+        try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(findSQL);
+            preparedStatement.setInt(1, this.id);
+            
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                this.setNamajenisbarang(rs.getString("namajenisbarang"));
+                return true;
+            }
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(JenisBarang.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
+    public ArrayList<JenisBarang> search(String keyword){
+        ArrayList<JenisBarang> list = new ArrayList<>();
+        
+        String searchSQL = "SELECT * FROM jenisbarang WHERE namajenisbarang like ?" ;
+        
+        keyword = "%" + keyword + "%";
+        
+        this.database = new Database();
+        this.connection = this.database.getConnection();
+        
+        try{
+            PreparedStatement preparedStatement = this.connection.prepareStatement(searchSQL);
+            preparedStatement.setString(1, keyword);
+            ResultSet rs = preparedStatement.executeQuery();
+            
+            while(rs.next()){
+                JenisBarang jb = new JenisBarang();
+                jb.setId(rs.getInt("id"));
+                jb.setNamajenisbarang(rs.getString("namajenisbarang"));
+                
+                list.add(jb);
+            }
+            
+            return list;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(JenisBarang.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNamajenisbarang() {
+        return namajenisbarang;
+    }
+
+    public void setNamajenisbarang(String namajenisbarang) {
+        this.namajenisbarang = namajenisbarang;
+    }
+}
